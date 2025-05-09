f2b9367a3aeb50ffa3c86b548b38484f56279513
Kenneth Aguilar <kennethaguilar436@gmail.com>
2025-05-09 14:25:09 +0800

    Update_backend-AGUILAR

--- app.js
+++ app.js
@@ -40,8 +40,8 @@ const petRoutes = require('./routes/petRoutes');
 // Middleware
 app.use(logger('dev'));
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: false, limit: '10mb' }));
 app.use(cookieParser());
 
 // Serve static files from both public and frontend directories
